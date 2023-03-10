1cd9278b5519887d3ff3483749e15dfafa9e991f
MarioTataje <magotor1304@gmail.com>
2023-03-10 09:31:45 -0500

    feat: agregar swagger

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
-
 const {router: clientRoutes} = require("./src/routes/client");
-
+const swaggerUI = require("swagger-ui-express");
+const swaggerDocument = require('./src/config/swagger.json');
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/api/', clientRoutes);
+app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 const PORT = 4040;
 app.listen(PORT, () => {
