8586651fb8907237f9656776c7f3c47a3e8d7593
MarioTataje <magotor1304@gmail.com>
2023-03-13 13:07:45 -0500

    fix: mysql azure connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,16 @@
 const {Sequelize} = require('sequelize');
 
-const sequelize = new Sequelize('promart', 'root', 'pacheco98', {
-    host: '127.0.0.1',
+const sequelize = new Sequelize('promart', 'promart', 'Tataje98', {
+    host: 'promart.mysql.database.azure.com',
     dialect: 'mysql',
     // avoid created_at and updated_at
     timezone: '+08:00',
+    dialectOptions: {
+        encrypt: true,
+        ssl : {
+          rejectUnauthorized: false
+        }
+    },
     define: {
         timestamps: false
     },
