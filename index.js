0615c288a875998e54182a71ba067cff25b5e18e
MarioTataje <magotor1304@gmail.com>
2023-03-13 14:10:37 -0500

    fix: cambiar puerto

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ sequelize.sync().then( () =>
 app.use('/api/', clientRoutes);
 app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
-const PORT = 8080;
+const PORT = 80;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
